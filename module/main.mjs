8ddbe548664aec9273fe2f3818ff0327b68df913
yoshuu <appcs342@gmail.com>
2023-04-22 17:04:00 +0800

    add main fn

--- module/main.mjs
+++ module/main.mjs
@@ -1,3 +1,21 @@
 import { add } from "./add.mjs";
 
 console.log(add(10, 2));
+
+const snakeSpeed = 1;
+
+let lastRenderTime = 0;
+
+const main = (currentTime) => {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+
+  if (secondsSinceLastRender < 1 / snakeSpeed) {
+    return;
+  }
+
+  console.log("Render");
+  lastRenderTime = currentTime;
+};
+
+window.requestAnimationFrame(main);
