7493d71de889ccb19d916f9ff314062f3287223d
yoshuu <appcs342@gmail.com>
2023-04-22 18:58:27 +0800

    add grid fn

---
+++ module/grid.mjs
@@ -0,0 +1,17 @@
+const gridSize = 21;
+
+export const randomGridPosition = () => {
+  return {
+    x: Math.floor(Math.random() * gridSize) + 1,
+    y: Math.floor(Math.random() * gridSize) + 1,
+  };
+};
+
+export const outsideGrid = (position) => {
+  return (
+    position.x < 1 ||
+    position.x > gridSize ||
+    position.y < 1 ||
+    position.y > gridSize
+  );
+};
