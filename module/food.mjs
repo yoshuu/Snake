74f97255d80b08b1b4e16c4f4141758c4d30cbd0
yoshuu <appcs342@gmail.com>
2023-04-22 19:19:25 +0800

    finish snake

--- module/food.mjs
+++ module/food.mjs
@@ -1,10 +1,16 @@
 import { onSnake, expendSnake } from "./snake.mjs";
+import { randomGridPosition } from "./grid.mjs";
 
-let food = { x: 0, y: 0 };
+let food = { x: 10, y: 1 };
 
 const expansionRate = 1;
 
-export const update = () => {};
+export const update = () => {
+  if (onSnake(food)) {
+    expendSnake(expansionRate);
+    food = { x: 20, y: 10 };
+  }
+};
 
 export const draw = (board) => {
   const foodElement = document.createElement("div");
@@ -12,4 +18,13 @@ export const draw = (board) => {
   foodElement.style.gridColumnStart = food.x;
   foodElement.classList.add("food");
   board.appendChild(foodElement);
+};
+
+const getRandomFoodPosition = () => {
+  let newFoodPosition;
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition();
+  }
+
+  return newFoodPosition;
 };
--- module/main.mjs
+++ module/main.mjs
@@ -1,12 +1,29 @@
 import { add } from "./add.mjs";
+import {
+  update as snakeUpdate,
+  draw as snakeDraw,
+  snakeSpeed,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.mjs";
+
+import { outsideGrid } from "./grid.mjs";
+
+import { update as foodUpdate, draw as foodDraw } from "./food.mjs";
 
 console.log(add(10, 2));
 
-const snakeSpeed = 1;
-
+const board = document.querySelector(".snake");
 let lastRenderTime = 0;
+let gameOver = false;
 
 const main = (currentTime) => {
+  if (gameOver) {
+    if (confirm("失敗")) {
+      window.location = "/";
+    }
+    return;
+  }
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
 
@@ -16,6 +33,25 @@ const main = (currentTime) => {
 
   console.log("Render");
   lastRenderTime = currentTime;
+
+  update();
+  draw();
 };
 
 window.requestAnimationFrame(main);
+
+const update = () => {
+  snakeUpdate();
+  foodUpdate();
+
+  checkDeath();
+};
+const draw = () => {
+  board.innerHTML = "";
+  snakeDraw(board);
+  foodDraw(board);
+};
+
+const checkDeath = () => {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+};
